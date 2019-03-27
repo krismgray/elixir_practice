9b9f5502d6e9e90630cfd6ee1071dffcdf6f1547
krismgray <kris.m.gray@gmail.com>
2019-03-26 23:22:23 -0600

    learning elixir

---
+++ hole1.ex
@@ -0,0 +1,13 @@
+defmodule Golf do
+  def sum_array do
+    t = 0
+    a.each { |i| total += i.to_i }
+    [a,t]
+  end
+end
+
+translate to elixir. Create a method that takes in an array sums the array then returns the array and the sum.
+
+seconds golf hole.
+HOLE 2: Rock paper scissors
+Takes a user input the computer randomly chooses and the winner is output to the screen.
